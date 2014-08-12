dd74718b9afcf641a5dd22f57c75294c472caadc
Taylor Daw <tdaw61@gmail.com>
2014-08-11 22:14:18 -0700

    ajax posting start

--- app/controllers/userposts_controller.rb
+++ app/controllers/userposts_controller.rb
@@ -4,13 +4,20 @@ class UserpostsController < ApplicationController
   def create
     @userpost = current_user.userposts.build(userpost_params)
     @userpost.game_id = params[:game][:id]
+
+    @game = Game.find(params[:game][:id])
+    @user_feed_items = @game.userposts
+    @game_event_feed_items = @game.game_events
+    @feed_items = (@user_feed_items + @game_event_feed_items).sort_by(&:created_at).reverse
     respond_to do |format|
       if @userpost.save
         format.html { redirect_to request.referer, notice: 'post was successfully created.' }
+        format.js
         # format.json { render root_url, status: :created, location: @userpost }
       else
         format.html { render :back }
         format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.js
       end
     end
   end
