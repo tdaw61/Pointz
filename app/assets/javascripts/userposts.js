9ac989fc8775113e00d84d5755eea3b030cbffa5
Taylor Daw <tdaw61@gmail.com>
2014-12-14 20:33:33 -0800

    uploader with preview

---
+++ app/assets/javascripts/userposts.js
@@ -0,0 +1,27 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#img_prev')
+                .attr('src', e.target.result)
+                .width(75)
+                .height(90);
+            $('#img_prev').show()
+            $('.userpost-extras').show()
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function removePhoto(){
+    $("#img_prev").hide();
+    $('.userpost-extras').hide();
+    $('#img_prev').attr('src', '');
+    $('#lefile').val("");
+}
+
+$( "#img_prev_del" ).click(function() {
+    alert( "Handler for .click() called." );
+});
