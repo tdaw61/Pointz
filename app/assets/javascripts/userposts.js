753a12b00f647b0f46b62570f0698a3ae7ca1fdf
Taylor Daw <tdaw61@gmail.com>
2015-01-05 16:51:13 -0800

    comment updates, sorting and picture fixes

--- app/assets/javascripts/userposts.js
+++ app/assets/javascripts/userposts.js
@@ -1,25 +1,34 @@
-function readURL(input) {
+function readURL(input, id) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            $('#img_prev')
+            if(id)
+                ele = $('#userpost_'+id);
+            else
+                ele = $('#userpost-form');
+            ele.find("#img_prev")
                 .attr('src', e.target.result)
                 .width(75)
                 .height(90);
-            $('#img_prev').show();
-            $('.userpost-extras').show();
+            ele.find("#img_prev").show();
+            ele.find('.userpost-extras').show();
         };
 
         reader.readAsDataURL(input.files[0]);
     }
 }
 
-function removePhoto(){
-    $("#img_prev").hide();
-    $('.userpost-extras').hide();
-    $('#img_prev').attr('src', '');
-    $('#lefile').val("");
+function removePhoto(id){
+
+    if(id)
+        ele = $('#userpost_'+id);
+    else
+        ele = $('#userpost-form');
+    ele.find("#img_prev").hide();
+    ele.find('.userpost-extras').hide();
+    ele.find('#img_prev').attr('src', '');
+    ele.find('#lefile').val("");
 }
 
 $( "#img_prev_del" ).click(function() {
