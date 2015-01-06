80c76767d6f153f34c6f9bdd6f2d9125a56c7e3b
Taylor Daw <tdaw61@gmail.com>
2015-01-06 12:55:58 -0800

    league expand links formatting

--- app/assets/javascripts/leagues.js
+++ app/assets/javascripts/leagues.js
@@ -1,9 +1,14 @@
-//$('#form').submit(function() {
-//    $(this).find("button[type='submit']").prop('disabled',true);
-//});
-function disable() {
-
-}
 $('#submit').click(function(){
     this.prop("disabled",true)
-})
+});
+
+
+function collapse(type)
+{
+    $(document).ready(function ($) {
+        $("tr[data-link]").off("click").on('click', function () {
+            alert("yeah");
+            $.ajax(this.dataset["link"], {type: 'get'})
+        });
+    });
+}
