cd665923975086f2dea6d105aea9b9e8e1a69a9e
Taylor Daw <tdaw61@gmail.com>
2015-01-07 19:51:43 -0800

    adding position query back in

--- app/models/game.rb
+++ app/models/game.rb
@@ -32,13 +32,12 @@ class Game < ActiveRecord::Base
   end
 
   def position user
-    # @rank = Score.find_by_sql("select (select  count(*)+1 from scores as s2 where s2.points > s1.points and game_id = :game_id) as UserRank from scores as s1 WHERE game_id = :game_id and user_id = :user_id", {game_id: self.id, user_id: user.id})
-    # if @rank.empty?
-    #   "N/A"
-    # else
-    #   @rank[0].UserRank
-    # end
-    "N/A"
+    @rank = Score.find_by_sql ["select (select  count(*)+1 from scores as s2 where s2.points > s1.points and game_id = :game_id) as UserRank from scores as s1 WHERE game_id = :game_id and user_id = :user_id", {game_id: self.id, user_id: user.id}]
+    if @rank.empty?
+      "N/A"
+    else
+      @rank[0].UserRank
+    end
   end
 
   def active_event_votes(current_user_id)
