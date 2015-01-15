15305372bcb10609eed30e32656f87ca0cfd7727
Taylor Daw <tdaw61@gmail.com>
2015-01-14 23:39:44 -0800

    friendships completed, not 100% tested, minor fix to get event detail expansion working

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,15 +7,15 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
-  def avatar_for(object, type, css_class)
+  def avatar_for(object, type, css_class, size)
     if(object.picture.present?)
       case type
         when "small"
-          image_tag(object.picture.small_avatar.url.to_s, class: css_class, alt: object.name)
+          image_tag(object.picture.small_avatar.url.to_s, class: css_class, alt: object.name, size: size)
         when "medium"
-          image_tag(object.picture.medium_avatar.url.to_s, class: css_class)
+          image_tag(object.picture.medium_avatar.url.to_s, class: css_class, size: "#{size}")
         when "large"
-          image_tag(object.picture.large_avatar.url.to_s, class: css_class)
+          image_tag(object.picture.large_avatar.url.to_s, class: css_class, size: size)
         else
           image_tag(PictureUploader.default_avatar.medium_avatar.url)
       end
@@ -24,19 +24,19 @@ module UsersHelper
     end
   end
 
-  def friend_link search_user
+  def friend_link search_user, type
 
     if current_user.id == search_user.id
        #Do nothing
     elsif(current_user.pending_friends.include?(search_user))
-      link_to 'Cancel Friend Request',  friendships_path(id: search_user.id), method: :delete, remote: true, data: 'Are you sure you want to unfriend this person?', class: 'padding-left btn btn-small btn-danger', id: "friend_link_#{search_user.id}"
+      link_to 'Cancel Friend Request',  friendships_path(id: search_user.id, from: request.params[:action]), method: :delete, remote: true, data: 'Are you sure you want to unfriend this person?', class: 'padding-left btn btn-small btn-danger', id: "friend_link_#{search_user.id}"
     elsif(current_user.requested_friends.include?(search_user))
       content_tag :div, id: "friend_link_#{search_user.id}" do
-        html = link_to "Accept", accept_friendship_path(id: search_user.id, from: request.params[:action] ), method: :post, remote: true, class: "padding-left btn btn-small btn-danger margin-right", data: {disable_with: "Processing"}
-        html += link_to "Reject", friendship_path(sear), method: :delete, remote: true, class: "padding-left btn btn-small btn-danger", data: {disable_with: "Processing"}
+        html = link_to type == 'long' ? "Accept Friend Request" : "Accept", accept_friendship_path(id: search_user.id, from: request.params[:action] ), method: :post, remote: true, class: "padding-left btn btn-small btn-danger margin-right", data: {disable_with: "Processing"}
+        html += link_to type == 'long' ? "Reject Friend Request" : "Reject", friendship_path(id: search_user.id, from: request.params[:action]), method: :delete, remote: true, class: "padding-left btn btn-small btn-danger", data: {disable_with: "Processing"}
       end
     elsif(current_user.friends.include?(search_user))
-      link_to 'UnFriend',  friendship_path(id: search_user.id), method: :delete, remote: true, data: "Are you sure you want to unfriend this person?", class: "padding-left btn btn-small btn-danger", id: "friend_link_#{search_user.id}"
+      link_to 'UnFriend',  friendship_path(id: search_user.id, from: request.params[:action]), method: :delete, remote: true, data: "Are you sure you want to unfriend this person?", class: "padding-left btn btn-small btn-danger", id: "friend_link_#{search_user.id}"
     else
       link_to 'Add as Friend', friendships_path(id: search_user.id), method: :post, remote: true, class: "padding-left btn btn-small btn-danger", data: {disable_with: "Processing"}, id: "friend_link_#{search_user.id}"
     end
