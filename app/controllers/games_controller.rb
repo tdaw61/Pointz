15e0b313714ad37bc3253930948cdb7f841553bc
Taylor Daw <tdaw61@gmail.com>
2015-02-02 16:54:28 -0800

    Upgrade from bootstrap 2 to bootstrap 3 in progress

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -22,10 +22,8 @@ class GamesController < ApplicationController
   #TODO there are multiple n+1 issues here
   def show
     @scores = @game.ordered_scores
-    @game = Game.includes(:userposts, :users, {userposts: [:comments, :likes] }).where(id: @game.id)
-    @game = @game[0]
+    @game = Game.includes(:userposts, :users, {userposts: [:comments, :likes] }).where(id: @game.id).first
     @feed_items = @game.userposts.includes(:comments).paginate(page: params[:page], per_page: 15)
-    @event_votes = Array.new
     @event_votes = @game.active_event_votes(current_user.id)
     @comment = Comment.new
     @game_event = @game.game_events.build
