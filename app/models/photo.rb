aec9173fde61db048c9c8ed608403045cca5a37d
Taylor Daw <tdaw61@gmail.com>
2015-02-08 23:15:28 -0800

    make picture polymorphic & being jCrop modal cropping for user + league profile images.

---
+++ app/models/photo.rb
@@ -0,0 +1,16 @@
+class Photo < ActiveRecord::Base
+  belongs_to :picture, polymorphic: true
+  mount_uploader :picture, PictureUploader
+  crop_uploaded :picture
+
+  validates_presence_of :picture_id, :picture_type
+
+  def self.update_photo model, picture
+     if model.photo.nil?
+       model.create_photo(picture: picture)
+     else
+       photo = Photo.new(picture: picture)
+       model.update_attribute(:photo, photo)
+     end
+  end
+end
